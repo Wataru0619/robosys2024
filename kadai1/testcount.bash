52e8382c7c6cc3749df070a0d620dd1e9a057cac
Wataru Suenaga <s23c1072kg@s.chibakoudai.jp>
2024-11-28 21:51:02 +0900

    成功

--- kadai1/testcount.bash
+++ kadai1/testcount.bash
@@ -11,14 +11,17 @@ res=0
 #Test1
 output=$(echo "abbccc" | python3 count.py)
 
+output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e 's/^[ \t]*//;s/[ \t]*$//' -e '/^$/d')
+
 expected="Counter({'c': 3, 'b': 2, 'a': 1})"
 
-if [ "$output" != "$expected" ]; then
+if [ "$output_cleaned" != "$expected" ]; then
     ng "$LINENO"
 else
     echo "Test1 Passed"
 fi
 
 exit $res
+
 
 
