70560dbaf19745425bc1f413971abaf0e5ab1201
Wataru Suenaga <s23c1072kg@s.chibakoudai.jp>
2024-11-28 21:59:13 +0900

    2追加

--- kadai1/testcount.bash
+++ kadai1/testcount.bash
@@ -8,17 +8,24 @@ ng () {
 
 res=0
 
-#Test1
-output=$(echo "abbccc" | python3 count.py)
-
+#Test
+output=$(echo "" | python3 count.py)
 output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e 's/^[ \t]*//;s/[ \t]*$//' -e '/^$/d')
-
-expected="Counter({'c': 3, 'b': 2, 'a': 1})"
-
+expected="No input"
 if [ "$output_cleaned" != "$expected" ]; then
     ng "$LINENO"
 else
     echo "Test1 Passed"
+fi
+
+#Test2
+output=$(echo "abbccc" | python3 count.py)
+output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e 's/^[ \t]*//;s/[ \t]*$//' -e '/^$/d')
+expected="Counter({'c': 3, 'b': 2, 'a': 1})"
+if [ "$output_cleaned" != "$expected" ]; then
+    ng "$LINENO"
+else
+    echo "Test2 Passed"
 fi
 
 exit $res
