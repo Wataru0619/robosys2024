0033297e0422f669227308b323159f83390dc7ab
Wataru Suenaga <s23c1072kg@s.chibakoudai.jp>
2024-11-29 21:42:07 +0900

    簡略化

--- kadai1/testcount.bash
+++ kadai1/testcount.bash
@@ -29,33 +29,39 @@ else
 fi
 
 #Test3
-output=$(echo "aaa" | python3 ./count)
-output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e '/^$/d')
-expected="Counter({'a': 3})"
-if [ "$output_cleaned" != "$expected" ]; then
-    ng 
+output=$(python3 ./count "a")
+expected="Counter({'a': 1})"
+if [ "$output" != "$expected" ]; then
+    ng
 else
     echo "Test3 Passed"
 fi
 
 #Test4
-output=$(echo "hello world" | python3 ./count)
-output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e '/^$/d')
-expected="Counter({'l': 3, 'o': 2, 'h': 1, 'e': 1, ' ': 1, 'w': 1, 'r': 1, 'd': 1})"
-if [ "$output_cleaned" != "$expected" ]; then
-    ng 
+output=$(python3 ./count "aaa")
+expected="Counter({'a': 3})"
+if [ "$output" != "$expected" ]; then
+    ng
 else
     echo "Test4 Passed"
 fi
 
 #Test5
-output=$(echo "!@#$%^&*()" | python3 ./count)
-output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e '/^$/d')
-expected="Counter({'!': 1, '@': 1, '#': 1, '$': 1, '%': 1, '^': 1, '&': 1, '*': 1, '(': 1, ')': 1})"
-if [ "$output_cleaned" != "$expected" ]; then
+output=$(python3 ./count "hello world")
+expected="Counter({'l': 3, 'o': 2, 'h': 1, 'e': 1, ' ': 1, 'w': 1, 'r': 1, 'd': 1})"
+if [ "$output" != "$expected" ]; then
     ng
 else
     echo "Test5 Passed"
+fi
+
+#Test6
+output=$(python3 ./count "!@#$%^&*()")
+expected="Counter({'!': 1, '@': 1, '#': 1, '$': 1, '%': 1, '^': 1, '&': 1, '*': 1, '(': 1, ')': 1})"
+if [ "$output" != "$expected" ]; then
+    ng
+else
+    echo "Test6 Passed"
 fi
 
 exit $res
