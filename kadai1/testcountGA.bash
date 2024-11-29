972db45ae4ce61090a85ca3ffacf43e59c436d92
Wataru Suenaga <s23c1072kg@s.chibakoudai.jp>
2024-11-29 11:28:31 +0900

    作成

---
+++ kadai1/testcountGA.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2024 Wataru Suenaga
+# SPDX-License-Identifier: GPL-3.0-only
+
+
+ng () {
+    echo "error"
+    res=1
+}
+
+res=0
+
+#Test1
+output=$(echo "" | python3 ./kadai1/count.py)
+output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e '/^$/d')expected="No input"
+if [ "$output_cleaned" != "$expected" ]; then
+    ng
+else
+    echo "Test1 Passed"
+fi
+
+exit $res
+
