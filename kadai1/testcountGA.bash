b0c0da484aa6a68550e34948b16b1b9a5cd40e30
Wataru Suenaga <s23c1072kg@s.chibakoudai.jp>
2024-11-29 22:18:20 +0900

    標準入力

--- kadai1/testcountGA.bash
+++ kadai1/testcountGA.bash
@@ -19,7 +19,7 @@ else
 fi
 
 # Test2
-output=$(echo "a" | python3 ./kadai1/count)
+output=$(echo "a" |./kadai1/count)
 expected="Counter({'a': 1})"  
 if [ "$output" != "$expected" ]; then
     ng
@@ -28,30 +28,27 @@ else
 fi
 
 # Test3
-output=$(echo "aaa" | python3 ./kadai1/count)
-output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e '/^$/d')
+output=$(echo "aaa" |./kadai1/count)
 expected="Counter({'a': 3})"
-if [ "$output_cleaned" != "$expected" ]; then
+if [ "$output" != "$expected" ]; then
     ng
 else
     echo "Test3 Passed"
 fi
 
 # Test4
-output=$(echo "hello world" | python3 ./kadai1/count)
-output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e '/^$/d')
+output=$(echo "hello world" |./kadai1/count)
 expected="Counter({'l': 3, 'o': 2, 'h': 1, 'e': 1, ' ': 1, 'w': 1, 'r': 1, 'd': 1})"
-if [ "$output_cleaned" != "$expected" ]; then
+if [ "$output" != "$expected" ]; then
     ng
 else
     echo "Test4 Passed"
 fi
 
 # Test5
-output=$(echo "!@#$%^&*()" | python3 ./kadai1/count)
-output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e '/^$/d')
+output=$(echo "!@#$%^&*()" |./kadai1/count)
 expected="Counter({'!': 1, '@': 1, '#': 1, '$': 1, '%': 1, '^': 1, '&': 1, '*': 1, '(': 1, ')': 1})"
-if [ "$output_cleaned" != "$expected" ]; then
+if [ "$output" != "$expected" ]; then
     ng
 else
     echo "Test5 Passed"
