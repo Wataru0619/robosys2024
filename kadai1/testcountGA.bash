a049917fe7c2bb4afd01d0531c0ec52fe6461248
Wataru Suenaga <s23c1072kg@s.chibakoudai.jp>
2024-11-29 11:48:51 +0900

    tuika

--- kadai1/testcountGA.bash
+++ kadai1/testcountGA.bash
@@ -39,5 +39,15 @@ else
     echo "Test3 Passed"
 fi
 
+# Test4
+output=$(echo "hello world" | python3 ./kadai1/count.py)
+output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e '/^$/d')
+expected="Counter({'l': 3, 'o': 2, 'h': 1, 'e': 1, ' ': 1, 'w': 1, 'r': 1, 'd': 1})"
+if [ "$output_cleaned" != "$expected" ]; then
+    ng
+else
+    echo "Test4 Passed"
+fi
+
 exit $res
 
