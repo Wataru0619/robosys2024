645b934053818aad131466396a604479ef5ea555
Wataru Suenaga <s23c1072kg@s.chibakoudai.jp>
2024-11-29 11:43:19 +0900

    tuika

--- kadai1/testcountGA.bash
+++ kadai1/testcountGA.bash
@@ -12,16 +12,17 @@ res=0
 # Test1
 output=$(echo "" | python3 ./kadai1/count.py)
 output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e '/^$/d')
-expected="No input"
+expected="No input"  
 if [ "$output_cleaned" != "$expected" ]; then
     ng
 else
     echo "Test1 Passed"
 fi
 
-#Test2
+# Test2
 output=$(echo "a" | python3 ./kadai1/count.py)
-output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e '/^$/d')expected="Counter({'a': 1})"
+output_cleaned=$(echo "$output" | sed -e 's/Please enter text//' -e '/^$/d')
+expected="Counter({'a': 1})"  
 if [ "$output_cleaned" != "$expected" ]; then
     ng
 else
